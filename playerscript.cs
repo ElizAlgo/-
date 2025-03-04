d18405dd4d65bfc66c56f7f1ac714f3789d7ee00
ElizAlgo <elizavetaalgoritmika24@gmail.com>
2025-03-04 19:00:43 +0300

    Create playerscript.cs

---
+++ playerscript.cs
@@ -0,0 +1,14 @@
+public void TakeDamage(int damage)
+{
+   health -= damage;
+
+    if(health > 0)
+    {
+        audioSource.PlayOneShot(damageSound);
+    }
+    else
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
